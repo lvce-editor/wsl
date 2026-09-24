7fda14629c32a5184b644ce20f25f26502adde71
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-24 10:17:48 +0000

    Use WSL test page object in e2e tests

--- packages/e2e/src/wsl.quickpick.ts
+++ packages/e2e/src/wsl.quickpick.ts
@@ -2,12 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'wsl.quickpick'
 
-export const test: Test = async ({ ActivityBar, expect, Extension, Locator, QuickPick }) => {
-  await Extension.enableWorkspace('lvce.wsl')
-  await ActivityBar.handleExtensionsChanged()
-  await QuickPick.open()
-  await QuickPick.setValue('>WSL: Connect to WSL')
-
-  const connectCommand = Locator('.QuickPickItem', { hasText: 'WSL: Connect to WSL' })
-  await expect(connectCommand).toBeVisible()
+export const test: Test = async ({ expect, Wsl }) => {
+  await Wsl.enableExtension()
+  await Wsl.shouldHaveConnectCommand()
 }
