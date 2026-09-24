bcfbdf900b833f4df1d259a93df6189491ef16b6
Le Vivilet <levivilet@pm.me>
2026-09-24 06:00:13 -0700

    Add WSL: Show Log quickpick command (#14)

--- packages/extension/src/parts/Main/Main.ts
+++ packages/extension/src/parts/Main/Main.ts
@@ -1,7 +1,10 @@
-import { activate as activateExtensionApi, registerCommand, registerFileSystemProvider } from '@lvce-editor/api'
+import { activate as activateExtensionApi, createOutputChannel, registerCommand, registerFileSystemProvider } from '@lvce-editor/api'
 import * as Connect from '../Connect/Connect.ts'
 import { fileSystem } from '../FileSystem/FileSystem.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
+import * as ShowLog from '../ShowLog/ShowLog.ts'
+
+const output = createOutputChannel('wsl')
 
 const state = {
   activated: false,
@@ -19,6 +22,11 @@ export const activate = async (): Promise<void> => {
       execute: Connect.connect,
       id: 'wsl.connect',
     })
+    registerCommand({
+      execute: ShowLog.showLog,
+      id: 'wsl.showLog',
+    })
+    await output.appendLine('WSL extension activated')
   } catch (error) {
     state.activated = false
     await Rpc.dispose()
